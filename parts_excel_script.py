6fa1de7e7d0961fe3af0b7d0837fa161a9babf0e
Keithg33 <keith.gordon9@gmail.com>
2020-06-08 15:57:53 -0700

    made class and methods and interface, mostly done

--- parts_excel_script.py
+++ parts_excel_script.py
@@ -2,97 +2,89 @@ from openpyxl import load_workbook
 import sys
 
 input_accept = False
+godzilla_parts_workbook ='parts_list.xlsx'
 
+# Lets have some class and make a class, rather than having a nasty long script that does everything...all puns intended of course
+class excelomator:
+    # Initialize the workbook and excel sheet we're working on
+    def __init__(self,wbook):
+        self.godzilla_workbook = load_workbook(wbook)
+        self.ws = self.godzilla_workbook.active
 
-godzilla_parts_workbook = load_workbook('parts_list.xlsx')
-ws = godzilla_parts_workbook.active
+    # This method will ask for the part, check if it's correct, and then add it to the spreadsheet. On the spreadsheet columns 2,3,4
+    # correspond to Brand, Part, and Purchase Price
+    def add_part(self):
+        while True:
+            # Get me inputs
+            part_string = input("Add part using 'Brand, Part, Purchase $' format:  ")
+            part_list = part_string.split(", ")
 
+            # Make sure I didn't ... up
+            print("You inputted: \n   Brand: " + part_list[0] + "\n   Part: " + part_list[1] + "\n   Purchase Price: " + part_list[2])
 
-# Lets split this up into methods for different actions.
-def add_part():
-    global input_accept
-    global godzilla_parts_workbook
-    global ws
+            keep_or_not = input("Do you want to keep this input? Answer y/n\n")
 
-    while input_accept == False:
-        # Get me inputs
-        part_string = input("Ready to go, add part using 'Brand, Part, Purchase $' format:  ")
-        part_list = part_string.split(", ")
+            if keep_or_not == "y":
+                break
+            elif keep_or_not != "y" and input_accept != "n":
+                ("Input error, please type 'y' for yes and 'n' for no...Input part again\n")
 
-        # Make sure I didn't ... up
-        print("You inputted: \nBrand: " + part_list[0] + "\nPart: " + part_list[1] + "\nPurchase Price: " + part_list[2])
+        brand = part_list[0]
+        part = part_list[1]
+        price = part_list[2]
 
-        keep_or_not = input("Do you want to keep this input? Answer y/n\n ")
+        max_row = 200
+        added=False
 
-        if keep_or_not == "y":
-            input_accept = True
-            break
-        elif keep_or_not == "n":
-            continue
-        elif keep_or_not != "y" or input_accept != "n":
-            ("Input error, please type 'y' for yes and 'n' for no...Try again\n ")
+        for row in range (6,max_row):
+            for col in range(2, 5):
+                cell = self.ws.cell(column=col, row=row)
 
-    input_accept = False
+                if cell.value is None:
+                    added=True
+                    if col == 2:
+                        print("changing cell valu for brand")
+                        cell.value = brand
+                    elif col == 3:
+                        cell.value = part
+                    elif col == 4:
+                        cell.value = int(price)
+            if added:
+                break
+            
+        
+        # Now sum up price
+        self.ws["F5"] = "=SUM(D6:D2000)"
 
-    brand = part_list[0]
-    part = part_list[1]
-    price = part_list[2]
+    # Adios
+    def peace_out(self):
+        print("Nos vemos cuando gastas mas dinero ;)")
 
-    MAX_ROW = 20
+        self.godzilla_workbook.save("parts_list.xlsx")
+        print("Workbook Saved")
+        exit()
 
-    for col in range (2,5):
-        for row in range(6, MAX_ROW):
-            cell = ws.cell(column=col, row=row, value = "")
+    # See the sad truth on how much I've spent
+    def check_damage(self):
+        money_spent = self.ws.cell(5,6).value
+        print("You've spent way too much: ${}".format(money_spent))
 
-            if cell.value is None or cell.value == "":
-                if col == 2:
-                    cell.value = brand
-                    break
-                elif col == 3:
-                    cell.value = part
-                    break
-                elif col == 4:
-                    cell.value = price
-                    break
-    
-    # Now sum up price
-    ws["F5"] = "=SUM(D6:D2000)"
-
-def peace_out():
-    print("Nos vemos cuando gastas mas dinero ;)")
-    exit()
-
-def check_damage():
-    global ws
-    money_spent = ws['F5']
-    print("You've spent: ${}".format(money_spent))
-
-def chooser(choice):
-    switcher = {
-                0: add_part,
-                2: check_damage,
-                3: peace_out
-                }
-    func = switcher.get(choice, lambda:'Invalid, choose from the listed choices')
-    return func()
+    # Dictionary to decide which method to run.
+    def chooser(self,choice):
+        switcher = {
+                    1: self.add_part,
+                    2: self.check_damage,
+                    0: self.peace_out
+                    }
+        func = switcher.get(choice, lambda:'Invalid, choose from the listed choices')
+        return func()
     
 
 # Now we run the interface
-if not godzilla_parts_workbook:
-    print("Excel sheet didn't load, better figure that out buddy guy")
-    exit()
-else:
-    while True:
-        action_choice = input("Welcome to the excel-omator...how would you like to proceed?\n\n 1 - Add part \n 2 - See spending total \n 3 - Exit")
-        chooser(action_choice)
+interface = excelomator(godzilla_parts_workbook)
+print("Welcome to the interface...")
 
+while True:
+    action_choice = int(input("What would you like to do?\n\n 1 - Add part \n 2 - See spending total \n 0 - Save and Exit\n\nInput: "))
 
-
-
-    
-
-    
-
-    # Got the info we need, time to edit excel sheet. Brand is ColB, part is ColC, price ColD
-godzilla_parts_workbook.save("parts_list.xlsx")
-print ("Workbook Saved")
+    choice = interface.chooser(action_choice)
